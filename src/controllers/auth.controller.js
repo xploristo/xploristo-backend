987ed0f67d9137821475aa0ca540d8b5e3470cfa
Celia Velasco <24230339+celiavelmar@users.noreply.github.com>
2022-10-02 17:03:34 +0200

    fix: Set HttpOnly cookie with SameSite set to 'None'
    Front-end and back-end are hosted in different domains.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,18 @@
 import authService from '../services/auth.service.js';
 
-
 async function login(req, res) {
   const { email, password } = req.body;
 
   const { sessionToken, sessionTTL } = await authService.login(email, password);
 
-  // TODO Should I use HttpOnly and find other way of knowing whether the user still has a session in frontend?
-  res.setHeader('Set-Cookie', `xpl_sid=${sessionToken}; max-age=${sessionTTL}; Path=/; SameSite=Strict; Secure`).sendStatus(200);
+  res
+    .cookie('xpl_sid', sessionToken, {
+      maxAge: sessionTTL * 1000,
+      sameSite: 'None',
+      secure: true,
+      httpOnly: true,
+    })
+    .sendStatus(200);
 }
 
 async function setPassword(req, res) {
